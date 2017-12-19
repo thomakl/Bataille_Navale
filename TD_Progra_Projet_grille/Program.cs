6c03bd81762ba1a0e216fad103f84e29d3b0c83e
tleduc <tleduc001@ensc.fr>
2017-12-19 19:10:08 +0100

    Initial check-in of module TD_Progra_Projet_grille

---
+++ TD_Progra_Projet_grille/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD_Progra_Projet_grille
+{
+    class Program
+    {
+
+        static void affichageCarte()
+        {
+            // Initialisation des noms des axes (horizontal et vertical)
+            string[] NomAxeHorizontal = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+            string[] NomAxeVertical = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+            int NumColonne = 0;
+
+            // Initialisation d'un caractère dans la grille
+            string vide = " ";
+
+            // Initialisation de la grille (10x10)
+            //// Initialisation des couples de lignes (démarcation de grille et intérieur de la grille)
+            Console.WriteLine();
+            for (int ligne = 0; ligne < 10; ++ligne)
+            {
+                // Initialisation des colonnes: démarcation extérieur
+                for (int colonne = 0; colonne < 10; ++colonne)
+                {
+                    if (colonne == 0)
+                    {
+                        Console.Write(" +---");
+                    }
+                    else { Console.Write("+---"); }
+                }
+                Console.Write("+");
+                Console.WriteLine();
+
+                // Initialisation des colonnes: intérieur
+                for (int colonne = 0; colonne < 10; ++colonne)
+                {
+                    if (colonne == 0)
+                    {
+                        Console.Write("{0}| {1} ", NomAxeVertical[NumColonne], vide);
+                        ++NumColonne;
+                    }
+                    else { Console.Write("| {0} ", vide); }
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            //// Initialisation des colonnes: démarcation extérieur finale
+            for (int colonne = 0; colonne < 10; ++colonne)
+            {
+                if (colonne == 0)
+                {
+                    Console.Write(" +---");
+                }
+                else { Console.Write("+---"); }
+            }
+            Console.Write("+");
+            Console.WriteLine();
+            // // Affichage de l'axe horizontal
+            for (int colonne = 0; colonne < NomAxeHorizontal.Length; ++colonne)
+            {
+                Console.Write("  {0} ", NomAxeHorizontal[colonne]);
+            }
+        }
+        static int[,] initCarte()
+        {
+            int[,] carte = new int[10, 10];
+            return carte;
+        }
+        static void Main(string[] args)
+        {
+            affichageCarte();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TD_Progra_Projet_grille/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("TD_Progra_Projet_grille")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bordeaux INP")]
+[assembly: AssemblyProduct("TD_Progra_Projet_grille")]
+[assembly: AssemblyCopyright("Copyright © Bordeaux INP 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("3269f513-4bcb-40b1-8612-4b19552785b7")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
